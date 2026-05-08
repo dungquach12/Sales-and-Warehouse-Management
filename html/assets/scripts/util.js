b57c0fe6240ee6ef745feb096f71ca78ae319745
dungquach12 <dungquach12.com@gmail.com>
2026-05-08 09:56:10 +0700

    export showLoadingState function

--- html/assets/scripts/util.js
+++ html/assets/scripts/util.js
@@ -1,9 +1,7 @@
-function showLoadingState(isLoading, containerId) {
+export function showLoadingState(isLoading, containerId) {
     // Show spinner or skeleton screen
     const container = document.getElementById(containerId);
     if (container && isLoading) {
         container.innerHTML = '<div class="text-center py-5"><div class="spinner-border text-secondary"></div> <br> <small>Đang tải...</small> </div>';
     }
 }
-
-export { showLoadingState };
